a3c6b82c790f8652b692cd7cdddd4ccb415e6711
Jesus Perez <Jesus@localhost.localdomain>
2017-02-14 12:42:48 -0600

    Inicio

---
+++ public/js/comm.js
@@ -0,0 +1,27 @@
+var sCC = io.connect('http://localhost:8080',{'forceNew':true});
+
+sCC.on('msg',function(data){
+  console.log("Info : " + data);
+  render(data);
+});
+
+function render(data){
+  var html = data.map(function(data,i){
+    return(`<div>
+                  <strong>${data.autor}</strong>
+                  <em>${data.texto}</em>
+
+                </div>`);
+  }).join(" ");
+  document.getElementById("frameChat").innerHTML = html;
+}
+
+function addMsg(e){
+  var payload = {
+    autor : document.getElementById('usuario').value,
+    texto : document.getElementById('texto').value
+  };
+
+  sCC.emit('newMsg',payload);
+  return false;
+}
