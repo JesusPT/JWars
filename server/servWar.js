684044706f350fea03046fe59f366e2c32c3f27c
ShinigamiJE <shini.p.t.sis@gmail.com>
2017-02-16 23:38:49 -0600

    Servidor y vista cuadrados

--- server/servWar.js
+++ server/servWar.js
@@ -2,6 +2,15 @@ var express = require('express');
 var app = express();
 var server = require('http').Server(app);//usamos http para que funcione bien con socket.io(Pero le pasamos express)
 var io = require('socket.io')(server);
+var userid = 0;
+
+  var players = [{
+    id : userid,
+    name : "admin",
+    color : "#000000",
+    x : 0,
+    y : 0
+  }];
 
  var mensajes = [{
    id : 1,
@@ -18,14 +27,67 @@ app.get('/',function(request, response){
 });
 
 io.on('connection',function(socket){
-  console.log("Nueva Conexion");
   socket.emit('msg',mensajes);
 
   socket.on('newMsg',function(data){
     mensajes.push(data);
     io.sockets.emit('msg',mensajes);
   });
+  socket.on('join',function(user){
+    players.push({
+      id : ++userid,
+      name : user,
+      color : "#"+((1<<24)*Math.random()|0).toString(16),
+      x : 20,
+      y : 20
+    });
+    socket.emit('accept',userid);
+    console.log("Nuevo Usuario ->" + userid);
+    console.log("Total usuario -> " + players.length);
+    io.sockets.emit('newPlayer',valSend(players));
+  });
+
+  socket.on('move',function(data){
+    console.log("Usuario " + data.id + " se mueve a " + data.dir);
+    index = indexId(data.id);
+    if(data.dir=="up"){
+      players[index].y-=5;
+    }else if(data.dir=="down"){
+      players[index].y+=5;
+    }else if(data.dir=="left"){
+      players[index].x-=5;
+    }else if(data.dir=="right"){
+      players[index].x+=5;
+    }else{
+      console.log("error de movimiento " + data.dir + " usuario " + data.id);
+    }
+
+    io.sockets.emit('update',valSend(players));
+  });
 });
+
+function valSend(listPlayers){
+  var poscol=[{}];
+  players.forEach(function(data,i){
+    poscol.push({
+      id : data.id,
+      x : data.x,
+      y : data.y,
+      color : data.color
+    });
+  });
+  return poscol;
+}
+
+function indexId(id){
+  index = 0;
+  players.forEach(function(data, i){
+    if(id = data.id){
+      index = 1;
+    }
+  });
+  return index;
+}
 
 server.listen(8080,function(){
   console.log("Server...[OK]");
